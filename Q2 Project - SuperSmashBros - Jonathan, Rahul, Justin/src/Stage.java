ca99b95524afc8fcde08d3e40877a207c37deb57
1911086 <1911086@01-19850.pusd.dom>
2021-11-04 14:03:02 -0700

    stage image

--- Q2 Project - SuperSmashBros - Jonathan, Rahul, Justin/src/Stage.java
+++ Q2 Project - SuperSmashBros - Jonathan, Rahul, Justin/src/Stage.java
@@ -1,5 +1,58 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
 
 public class Stage {
+	
+	private Image img; 	
+	private AffineTransform tx;
+
+	public Stage(int x, int y) {
+		img = getImage("/imgs/stage.png"); //load the image for Tree
+		tx = AffineTransform.getTranslateInstance(x, y );
+		init(x, y); 				//initialize the location of the image
+									//use your variables
+	}
+	
+	/* update variables here */
+	private void update() {
+		
+	}
+	
+	/* Drawing commands */
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		
+		
+		//call update to update the actual picture location
+		update();
+		
+		g2.drawImage(img, tx, null);
+		
+	}
+	
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		
+		//use THIS for scaling the stage
+		tx.scale(1, 1);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Background.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
 	
 
 }
